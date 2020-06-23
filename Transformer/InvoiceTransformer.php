46621ed49bc66375278c0c98a453244efd4d3cd9
Javi H. Gil <javihgil@gmail.com>
2020-06-23 19:16:22 +0200

    Create payment on invoice transformation

--- Transformer/InvoiceTransformer.php
+++ Transformer/InvoiceTransformer.php
@@ -2,9 +2,13 @@
 
 namespace Softspring\PlatformBundle\Stripe\Transformer;
 
+use Softspring\PaymentBundle\Manager\PaymentManagerInterface;
 use Softspring\PaymentBundle\Model\InvoiceInterface;
+use Softspring\PaymentBundle\Model\PaymentInterface;
 use Softspring\PlatformBundle\Model\PlatformObjectInterface;
+use Softspring\PlatformBundle\Stripe\Adapter\PaymentAdapter;
 use Softspring\PlatformBundle\Transformer\PlatformTransformerInterface;
+use Stripe\Charge;
 use Stripe\Invoice;
 
 class InvoiceTransformer extends AbstractPlatformTransformer implements PlatformTransformerInterface
@@ -16,6 +20,35 @@ class InvoiceTransformer extends AbstractPlatformTransformer implements Platform
         'uncollectible' => InvoiceInterface::STATUS_UNPAID,
         'void' => InvoiceInterface::STATUS_CANCELED,
     ];
+
+    /**
+     * @var PaymentManagerInterface|null
+     */
+    protected $paymentManager;
+
+    /**
+     * @var PaymentAdapter|null
+     */
+    protected $paymentAdapter;
+
+    /**
+     * @var PaymentTransformer|null
+     */
+    protected $paymentTransformer;
+
+    /**
+     * InvoiceTransformer constructor.
+     *
+     * @param PaymentManagerInterface|null $paymentManager
+     * @param PaymentAdapter|null          $paymentAdapter
+     * @param PaymentTransformer|null      $paymentTransformer
+     */
+    public function __construct(?PaymentManagerInterface $paymentManager, ?PaymentAdapter $paymentAdapter, ?PaymentTransformer $paymentTransformer)
+    {
+        $this->paymentManager = $paymentManager;
+        $this->paymentAdapter = $paymentAdapter;
+        $this->paymentTransformer = $paymentTransformer;
+    }
 
     public function supports($invoice): bool
     {
@@ -83,6 +116,25 @@ class InvoiceTransformer extends AbstractPlatformTransformer implements Platform
         $invoice->setDate(\DateTime::createFromFormat('U', $stripeInvoice->created));
         $invoice->setTotal($stripeInvoice->total/100);
         $invoice->setCurrency(strtoupper($stripeInvoice->currency));
+
+        $stripeChargeId = $stripeInvoice->charge;
+        if ($this->paymentTransformer && $stripeChargeId) {
+            // find payment
+            $charge = $invoice->getPayments()->filter(function (PlatformObjectInterface $payment) use ($stripeChargeId) {
+                return $payment->getPlatformId() == $stripeChargeId;
+            })->first();
+
+            if ($charge instanceof PaymentInterface) {
+                $this->paymentAdapter->get($charge);
+            } else {
+                /** @var PlatformObjectInterface|PaymentInterface $charge */
+                $charge = $this->paymentManager->createEntity();
+                $charge->setType(PaymentInterface::TYPE_CHARGE);
+                $charge->setPlatformId($stripeChargeId);
+                $this->paymentAdapter->get($charge);
+                $invoice->addPayment($charge);
+            }
+        }
 
         return $invoice;
     }
